e0522728d4cf8519a6af2e5506a094cd52e3dfc8
unknown <zienzidan41@gmail.com>
2023-09-21 16:53:13 +0700

    drive.ae

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DriveStorage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Folders_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Folders_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CircularProgressBar_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuButton_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Folders_Loaded_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuButton_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MediumCard_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MediumCard_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MediumCard_Loaded_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CustomList_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
